e84cfe40105213d2c1afb65bfa086cd89df78234
sher <miansherzaman7@gmail.com>
2022-06-21 18:34:59 +0500

    This is my 1st commit

---
+++ src/components/home.vue
@@ -0,0 +1,21 @@
+<template>
+  
+  <div class="Sidebar">
+      <h1>Dashboard</h1>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: "home-component",
+};
+</script>
+
+<style>
+.main01 {
+  background-color: black !important;
+  color: white;
+}
+</style>
---
+++ src/components/rightsection.vue
@@ -0,0 +1,15 @@
+<template>
+
+<div class="Main">
+    <h1>rightsection</h1> 
+    <h3>hello everyone  kisa ho </h3>
+</div>
+
+
+</template>
+
+<script>
+export default {
+    name : 'rightsection'
+}
+</script>
